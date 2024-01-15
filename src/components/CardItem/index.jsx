310aed8564b6f1bb889897fbc6f7659bdc38524a
Poliana <polianariveiro@gmail.com>
2024-01-15 19:32:47 -0300

    subindo alguns ajustes no css

--- src/components/CardItem/index.jsx
+++ src/components/CardItem/index.jsx
@@ -1,14 +1,32 @@
 import styles from "./CardItem.module.css";
+import { GiDiamonds } from "react-icons/gi";
 
-export default function CardItem( { name, image, status, species, type}) {
+export default function CardItem({ name, image, status, species, type }) {
   return (
     <>
       <div className={styles.cardItem}>
-      <img src={image} alt="" />
+        <img src={image} alt="" />
         <h1> {name} </h1>
-        <p>{status} </p>
-        <p>{species}</p>
-        <p>{type}</p>
+        <div className={styles.info}>
+          <GiDiamonds size={12} />
+          <p>{status}</p>
+        </div>
+
+        <div className={styles.info}>
+          <GiDiamonds size={12} />
+          <p>{species}</p>
+        </div>
+
+        {type && (
+          <div className={styles.info}>
+            <GiDiamonds size={12} />
+            <p>{type}</p>
+          </div>
+        )}
+
+        {/* <p>Status:</p><span>{status} </span>
+        <p>Espécie:</p><span>{species} </span>
+        <p>Tipo:</p><span>{type} </span> */}
 
         <button className={styles.button}></button>
       </div>
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,23 @@
-import styles from './Header.module.css';
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-
-
-    return (
-    
+  return (
     <header className={styles.header}>
+      <ul className={styles.list}>
+        <li className={styles.item}>
+          <Link to="/" className={styles.link}>
+            Home
+          </Link>
+        </li>
+        <li className={styles.item}>
+          <Link to="/" className={styles.link}>
+            Sobre
+          </Link>
+        </li>
+      </ul>
 
-        <h1 className={styles.title}>Rick and Morty</h1>
-
+      <h1 className={styles.title}>Rick and Morty</h1>
     </header>
-    
-    
-    )
-    
-    
-    }
+  );
+}
