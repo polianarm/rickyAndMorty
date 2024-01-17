2b852eb010bee95960ddcb2983c8d09192c746a9
Poliana <polianariveiro@gmail.com>
2024-01-17 20:25:19 -0300

    subindo edicoes no css em detalhe e ajeitando o footer

--- src/components/DetalhePersonagens/index.jsx
+++ src/components/DetalhePersonagens/index.jsx
@@ -3,27 +3,55 @@ import styles from "./DetalhePersonagens.module.css";
 export default function DetalhePersonagens({ personagens, locations }) {
   return (
     <section className={styles.gridContainer}>
-    <div className={styles.grid1ImagemLeft}>
-      <img src={personagens.image} alt="" />
-    </div>
+      <div className={styles.grid1ImagemLeft}>
+        <img src={personagens.image} alt="" />
+      </div>
 
-    <div className={styles.grid2Info}>
-      <h1>{personagens?.name}</h1>
-      {personagens.status && <h2>Status: {personagens.status}</h2>}
-      {personagens.species && <h2>Espécie: {personagens.species}</h2>}
-      {personagens.type && <h2>Tipo: {personagens.type}</h2>}
-      {personagens.gender && <h2>Gênero: {personagens.gender}</h2>}
+      <div className={styles.grid2Info}>
+        <h1>{personagens?.name}</h1>
+        <div className={styles.gridQuestions}>
+        <h2>Character Biography</h2>
+          {personagens.status && (
+            <p>
+              Status : <span>{personagens.status}</span>
+            </p>
+          )}
+          {personagens.species && (
+            <p>
+              Specie : <span>{personagens.species}</span>
+            </p>
+          )}
+          {personagens.gender && (
+            <p>
+              Gender : <span>{personagens.gender}</span>
+            </p>
+          )}
+          {personagens.type && (
+            <p>
+              What is this ? <span>{personagens.type}</span>
+            </p>
+          )}
 
-      {personagens.location?.name && <h2>Local onde habita: {personagens.location.name}</h2>}
-
-      {personagens.origin?.name && <h2>Local de origem: {personagens.origin.name}</h2>}
-      {locations.dimension && <h2>Dimensão: {locations.dimension}</h2>}
-      {locations.type && <h2>Tipo: {locations.type}</h2>}
-      {locations.name && <h2>Nome: {locations.name}</h2>}
-    </div>
-    <div className={styles.grid1ImagemRight}>
-      <img src={personagens.image} alt="" />
-    </div>
-  </section>
+          {personagens.location?.name && (
+            <p>
+              Origin : <span>{personagens.location.name}</span>
+            </p>
+          )}
+          {locations.type && (
+            <p>
+              Where : <span>{locations.type}</span>
+            </p>
+          )}
+          {locations.dimension && (
+            <p>
+              Dimension : <span>{locations.dimension}</span>
+            </p>
+          )}
+        </div>
+      </div>
+      <div className={styles.grid1ImagemRight}>
+        <img src={personagens.image} alt="" />
+      </div>
+    </section>
   );
 }
